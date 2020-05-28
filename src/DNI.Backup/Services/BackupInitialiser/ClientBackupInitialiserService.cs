c5253dbb43b9a09f6936bc7658374152d9260ca5
Chris Sebok <chris@shadowmoses.co.uk>
2020-05-28 20:48:48 +0100

    Removed backup pattern from .gitignore

---
+++ src/DNI.Backup/Services/BackupInitialiser/ClientBackupInitialiserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DNI.Backup.Services.BackupInitialiser {
+    public class ClientBackupInitialiserService : IClientBackupInitialiserService {
+        public ClientBackupInitialiserService() {
+        }
+
+        public async Task ProcessBackup(string[] configs) {
+            // TODO: Iterate through configs
+            // TODO: Validate / read configs against backupConfigs in client configuration file (.Net Core configuration reader?)
+            // TODO: Enumerate list of local files to be backed up
+            // TODO: Send file list to server (compress?)
+            // TODO: Server responds with file list status and / or deltas
+            //      new (doesn't exist on server)
+            //      exists (server sends file signature back)
+            //      deleted from source (exists on server, but not on client)
+
+            // TODO: For each file in server response
+            //      if new, send file
+            //      if exists, generate delta and send to server -> Server then applies delta patch
+            //      if deleted, tell server to delete if DeleteDestinationFilesNotInSource set to true
+            // TODO: Await server response and if successful, return true
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IClientBackupInitialiserService {
+        Task ProcessBackup(string[] configs);
+    }
+}
---
+++ test/DNI.Backup.Test/Services/BackupInitialiser/BackupInitialiserServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+using AutoFixture;
+using AutoFixture.AutoMoq;
+
+using DNI.Backup.Services.BackupInitialiser;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DNI.Backup.Test.Services.BackupInitialiser {
+    [Trait(TestTraits.TEST_TYPE, TestTraits.UNIT)]
+    public class BackupInitialiserServiceTests {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IFixture _fixture = new Fixture().Customize(new AutoMoqCustomization {ConfigureMembers = true});
+
+        public BackupInitialiserServiceTests(ITestOutputHelper _output) {
+            this._output = _output;
+        }
+
+        private IClientBackupInitialiserService GetService() {
+            return new ClientBackupInitialiserService();
+        }
+
+        [Fact]
+        public async Task Test() {
+            // Arrange
+            var service = GetService();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
